a06e8495284e540ef8d86aacd120c3508b811350
Fretz <Fretz>
2017-03-15 12:49:35 +0800

    update

---
+++ about-us.php
@@ -0,0 +1,196 @@
+<?php /* Template Name: About */ ?>
+
+<?php get_header(); ?>
+
+<div id="about-page">
+
+    <div id="about-banner">
+        <div class="banner-bg"></div>
+        <div class="overlay">
+
+            <div class="container">
+
+                <h1>about artic</h1>
+
+            </div>
+
+        </div>
+    </div>
+
+    <div id="about-history">
+        <div class="overlay">
+
+            <div class="container">
+
+                <div class="row about-intro">
+                    <di class="col-sm-2 lead-seal">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/leaf-orange.svg">
+                    </di>          
+                    <di class="col-sm-10 intro-text">
+                        Since 1983 Arctic Wire Rope &amp; Supply has been leading the Pacific Northwest
+                    </di>      
+                </div>
+
+                <div class="intro-con-text">
+
+                    <p>In 1983 I got fired from my job so like many accidental entrepreneurs, started my own company. Over the last 34 years we have really tried to focus on giving superior service, having great product knowledge and a large inventory. We have steadily added new services and products.</p>
+
+                    <p>We have been the main rigging supplier to Alaska’s vast North Slope &amp; Cook Inlet oil fields ever since. We make lifting slings and bridles of every kind; wire rope, chain, Nylon web, polyester round and Dyneema high performance rope. In 2006 we received the Alaska Manufacture of the Year award.</p>
+                    
+                    <i class="icon-sprite icon-qoute"></i>
+
+                </div>
+
+                <div class="intro-owner">
+
+                    <div class="thumb"><img src="<?php echo get_template_directory_uri(); ?>/images/EricMcCallum.jpeg"></div>
+                    <strong>Eric McCallum</strong>
+                    <span>Founder &amp; CEO</span>
+
+                </div>
+
+
+            </div>
+
+        </div>
+    </div>
+
+    <div id="about-core">
+        <div class="overlay">
+
+            <div class="container">
+
+                <h2>our core values</h2>
+
+                <ul class="core-values">
+                    <li class="core-values-item">
+                        <div><i class="icon-sprite icon-users"></i></div>
+                    </li>
+                    <li class="core-values-item">
+                        <div><i class="icon-sprite icon-cloud"></i></div>
+                    </li>
+                    <li class="core-values-item">
+                        <div><i class="icon-sprite icon-gear"></i></div>
+                    </li>
+                    <li class="core-values-item">
+                        <div><i class="icon-sprite icon-graph-chart"></i></div>
+                    </li>
+                </ul>
+
+                <h3>customer services</h3>
+
+                <p> We believe if we take care of our customers and I take care of our employees, business will take will take care of itself. </p>
+
+            </div>
+
+        </div>
+    </div>
+
+    <div id="about-team">
+        <div class="container">
+
+            <div class="tagline">Our expert team is committed to delivering only the highest quality service</div>
+
+            <div class="row team-members">
+
+                <div class="col-sm-3">
+                    <div class="item-holder">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/sample-member-img.png">
+                        <div class="name">Mark Lamoureux</div>
+                        <div class="position">General Manager</div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="item-holder">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/sample-member-img.png">
+                        <div class="name">Jill Reeves</div>
+                        <div class="position">Sales Manager</div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="item-holder">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/sample-member-img.png">
+                        <div class="name">Scott Jenks</div>
+                        <div class="position">Inside Sales</div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="item-holder">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/sample-member-img.png">
+                        <div class="name">Philip West</div>
+                        <div class="position">Inside Sales &amp; IT</div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row team-members">
+
+                <div class="col-sm-3">
+                    <div class="item-holder">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/sample-member-img.png">
+                        <div class="name">Russell Forbes</div>
+                        <div class="position">Plant Superintendent</div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="item-holder">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/sample-member-img.png">
+                        <div class="name">Jeff Harrison</div>
+                        <div class="position">Rigging Foreman</div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="item-holder">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/sample-member-img.png">
+                        <div class="name">Jover Estrella</div>
+                        <div class="position">Sewing Foreman</div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="item-holder">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/sample-member-img.png">
+                        <div class="name">Alvin Mason</div>
+                        <div class="position">Rigging &amp; Sewing Dept.</div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row team-members">
+
+                <div class="col-sm-12">
+                    <div class="item-holder">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/sample-member-img.png">
+                        <div class="name">Eric McCallum</div>
+                        <div class="position">President &amp; bottle washer</div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+    <div id="about-dlcat">
+        <div class="container">
+
+            <div class="tagline">Download a PDF of Our Product Catalog</div>
+
+            <form>
+                <div class="row">
+                    <div class="col-sm-8">
+                        <input type="email" name="email" class="form-control" placeholder="Email Address">
+                    </div>
+                    <div class="col-sm-4 btn-submit">
+                        <button type="submit" class="btn btn-awr">Download Now</button>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </div>    
+    
+</div>
+
+<?php get_footer(); ?>
